dac4b66994c7f45fc25b106459cb862a0d16889e
Jauri da Cruz Junior <jauri.junior@novamerica.com.br>
2020-03-18 15:26:20 -0300

    Initial commit

---
+++ src/main/java/br/com/jauri/junior/SpringBootSocket2Application.java
@@ -0,0 +1,13 @@
+package br.com.jauri.junior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSocket2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSocket2Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jauri/junior/SpringBootSocket2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jauri.junior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSocket2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
